463bd811e83a123232ec1e92ecbab0e2e3bd3e3e
Matt Kadlubowski <xeho91@pm.me>
2022-02-02 13:28:13 +0800

    chore: 🔧  Configure base tools

---
+++ .eslintrc.cjs
@@ -0,0 +1,11 @@
+const config = {
+	extends: "@terminal-nerds/eslint-config",
+
+	env: {
+		// Specify the environment(s)
+		// https://eslint.org/docs/user-guide/configuring/language-options#specifying-environments
+		node: true,
+	},
+};
+
+module.exports = config;
---
+++ .prettierrc.cjs
@@ -0,0 +1,9 @@
+/** @type import("prettier").Options */
+const config = {
+	...require("@terminal-nerds/prettier-config"),
+
+	// tweak some options here, refer to the link below:
+	// https://prettier.io/docs/en/index.html
+};
+
+module.exports = config;
